ce2e67aae35d2d6c5c34e51198e5c5b41048f60e
alt-core <alt-core@users.noreply.github.com>
2026-08-11 20:43:05 +0900

    外部接続なしの標準回帰テストを整備する

--- test.sh
+++ test.sh
@@ -1,2 +1,27 @@
 #!/bin/sh
-python2 -m unittest discover
+set -eu
+
+export XSBOT_DEPLOY_ENV=test
+
+python3 -m unittest \
+    tests.test_settings_configuration \
+    tests.test_auth \
+    tests.test_expression \
+    tests.test_runtime_isolation \
+    tests.test_state_namespace \
+    tests.test_scenario_storage \
+    tests.plugin.test_google_sheets \
+    tests.plugin.test_chatgpt \
+    tests.plugin.test_liff \
+    tests.plugin.test_mock_line \
+    tests.plugin.test_images \
+    tests.plugin.test_twilio \
+    tests.plugin.test_line_contracts \
+    tests.test_task_client \
+    tests.test_auth_middleware \
+    tests.test_api_endpoints \
+    tests.test_group_message_batching \
+    tests.test_dashboard \
+    tests.test_initialization_boundaries \
+    tests.test_container_configuration \
+    tests.test_logparser
